4217dca184775b36e7971bc091ba84930ffb6735
Shivanshika Agarwal <113771299+ShivanshikaAgarwal1505@users.noreply.github.com>
2023-12-28 18:06:35 +0530

    Add files via upload

---
+++ Foot.php
@@ -0,0 +1,69 @@
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+<script>
+    AOS.init();
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('#dynamic').addClass('newClass');
+            $('#dynamic').removeClass('p-5');
+            $('#dynamic').addClass('p-3');
+        } else {
+            $('#dynamic').removeClass('newClass');
+            $('#dynamic').removeClass('p-3');
+            $('#dynamic').addClass('p-5');
+        }
+    });
+
+    const icons = document.querySelectorAll('.icon');
+    icons.forEach(icon => {
+        icon.addEventListener('click', (event) => {
+            icon.classList.toggle("open");
+        });
+    });
+
+    function openTab(tabName) {
+        let i;
+        let tabContent;
+
+        tabContent = document.getElementsByClassName("tab-content");
+
+        for (i = 0; i < tabContent.length; i++) {
+            tabContent[i].style.display = "none";
+        }
+
+        document.getElementById(tabName).style.display = "flex";
+    }
+
+    // This needs to DRY'ed up so it can be used with a CMS
+
+    let progressEl = document.getElementById("progress");
+    progressEl.addEventListener("click", function() {
+        openTab("pro")
+    }, false);
+    let wishlistEl = document.getElementById("wishlist");
+    wishlistEl.addEventListener("click", function() {
+        openTab("wish")
+    }, false);
+    let cartEl = document.getElementById("cart");
+    cartEl.addEventListener("click", function() {
+        openTab("ca")
+    }, false);
+    let settingsEl = document.getElementById("settings");
+    settingsEl.addEventListener("click", function() {
+        openTab("set")
+    }, false);
+</script>
+</body>
+
+</html>
---
+++ footer.php
@@ -0,0 +1,102 @@
+ 
+<div class="container-fluid p-0">
+
+<!-- Footer -->
+<footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+
+  <section class="d-flex justify-content-between p-4" style="background-color: #1c2331; border-bottom: 2px solid #fff;">
+    <div class="me-5">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    
+    <div>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="text-white me-4" style="text-decoration: none;">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </section>
+ 
+  <section>
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold">SKILL VANTAGE</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px"/>
+          <p>
+            Make learning easy and fun.
+          </p>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold">Major Courses</h6>
+          <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px"/>
+          <p>
+            <a href="#!" class="text-white">MDBootstrap</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">MDWordPress</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">BrandFlow</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">Bootstrap Angular</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold">Useful links</h6>
+          <hr
+              class="mb-4 mt-0 d-inline-block mx-auto"
+              style="width: 60px; background-color: #7c4dff; height: 2px"
+              />
+          <p>
+            <a href="#!" class="text-white">Your Account</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!" class="text-white">Help</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold">Contact</h6>
+          <hr
+              class="mb-4 mt-0 d-inline-block mx-auto"
+              style="width: 60px; background-color: #7c4dff; height: 2px"
+              />
+          <p><i class="fas fa-home mr-3"></i>Uttar Pradesh, Ayodhya, India</p>
+          <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2023 Copyright:
+    <a class="text-white" href="">skillvantage.com</a>
+  </div>
+</footer>
+</div>
